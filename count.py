16892ff56a9943ff177e35977de646178aed698c
Seth Kahn <kahns729@gmail.com>
2016-12-05 18:31:35 -0500

    checked file existence

--- count.py
+++ count.py
@@ -1,13 +1,29 @@
+import os.path
+
+
+
 def increment_count():
+	if os.path.isfile('./count.txt'):
+		filename = './count.txt'
+	else:
+		filename = '/var/www/html/app/count.txt'
 	current_count = get_count()
-	with open('./count.txt', 'w+') as f:
+	with open(filename, 'w+') as f:
 		f.write(str(current_count + 1))
 
 def decrement_count():
+	if os.path.isfile('./count.txt'):
+		filename = './count.txt'
+	else:
+		filename = '/var/www/html/app/count.txt'
 	current_count = get_count()
-	with open('./count.txt', 'w+') as f:
+	with open(filename, 'w+') as f:
 		f.write(str(current_count - 1))
 
 def get_count():
-	with open('./count.txt', 'r') as f:
+	if os.path.isfile('./count.txt'):
+		filename = './count.txt'
+	else:
+		filename = '/var/www/html/app/count.txt'
+	with open(filename, 'r') as f:
 		return int(f.read().rstrip())
