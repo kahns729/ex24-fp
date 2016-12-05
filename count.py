22418dcb31965f97eb4a45696e0e09a06e632ab2
Seth Kahn <kahns729@gmail.com>
2016-12-05 18:12:56 -0500

    edited filepath, hopefully will work with apache

--- count.py
+++ count.py
@@ -1,13 +1,13 @@
 def increment_count():
 	current_count = get_count()
-	with open('count.txt', 'w+') as f:
+	with open('./count.txt', 'w+') as f:
 		f.write(str(current_count + 1))
 
 def decrement_count():
 	current_count = get_count()
-	with open('count.txt', 'w+') as f:
+	with open('./count.txt', 'w+') as f:
 		f.write(str(current_count - 1))
 
 def get_count():
-	with open('count.txt', 'r') as f:
+	with open('./count.txt', 'r') as f:
 		return int(f.read().rstrip())
