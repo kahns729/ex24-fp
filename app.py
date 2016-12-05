a3883e98d98c567d03f13e47632b2181aa09fb3e
Seth Kahn <kahns729@gmail.com>
2016-12-05 18:33:45 -0500

    removed extra output

--- app.py
+++ app.py
@@ -7,22 +7,6 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def hello_world():
-	   # Advanced usage:
-	# editing the 'dirnames' list will stop os.walk() from recursing into there.
-
-	for dirname, dirnames, filenames in os.walk('.'):
-		if '.git' in dirnames:
-			# don't go into any .git directories.
-			dirnames.remove('.git')
-		# print path to all subdirectories first.
-		for subdirname in dirnames:
-			print(os.path.join(dirname, subdirname))
-
-		# print path to all filenames.
-		for filename in filenames:
-			print(os.path.join(dirname, filename))
-
-
 	current_count = get_count()
 	return render_template('index.html', num=current_count)
 
