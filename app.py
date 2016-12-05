f067cbec167444fa9996b0d37baaa0503dbc5793
Seth Kahn <kahns729@gmail.com>
2016-12-05 15:04:58 -0500

    added count

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return render_template('index.html')
+  return render_template('index.html', num=0)
 
 if __name__ == '__main__':
   app.run()
